02a14f7638f6dd016581de9a5bf2fd2ebe637bc6
Martin <46757310+MartinAlias@users.noreply.github.com>
2019-02-25 11:36:58 -0500

    Add files via upload

--- GuessingGame/GuessingGame.java
+++ GuessingGame/GuessingGame.java
@@ -19,24 +19,53 @@
  		int num;									//Variable decloration-int
  		double secretnum;							//Variable decloration-double
 
- 		System.out.print ("Enter a number between 1 and 20: ");		//System output for num
+ 		System.out.print ("Enter a number between 1 and 50: ");		//System output for num
  		num = input.nextInt();										//user input for variable num
 
- 		secretnum = ((int)(20*Math.random() + 1));					//Random number generation
+ 		secretnum = ((int)(50*Math.random() + 1));					//Random number generation
 
- 		while (num != secretnum) {
- 			if (num >== secretnum) {
- 				System.out.println ("Too High");
+ 		do{															//do-while statement
+ 			if (num >= secretnum) {									//nested if statement with condition
+ 				System.out.println ("Too High");					//System output
  			}
- 			if (num <= secretnum) {
- 				System.out.println ("Too Low");
+ 			if (num <= secretnum) {									//nested if statement with condition
+ 				System.out.println ("Too Low");						//System output
  			}
- 			System.out.print ("Enter a number between 1 and 20: ");
- 		num = input.nextInt();
+ 			if (num > 50){											//nested if statement with condition
+ 			System.out.println("Error: Enter a valid number");		//System output for errors
+ 			break;													//break statement to stop program after errors
  		}
- 		if (num == secretnum) {
- 			System.out.println ("YOU WIN!");
+ 			System.out.print ("Enter a number between 1 and 50: ");	//System output
+ 		num = input.nextInt();										//user input
+ 		} while (num != secretnum);									//while statement with condition
+
+ 		input.close();												//Closing scanner
+
+ 		if (num == secretnum) {										//if statement with condition
+ 			System.out.println ("YOU WIN!");						//System output
  		}
- 		input.close();
- 	}
- }
+	}
+}
+
+	/*
+	 *	This approach works as you are simply finding a middle value
+	 *in a set of numbers seeing wether or not that mid point is too
+	 *high or too low. If its too high you can disregard all numbers
+	 *after it including the number itself, and if its too low all
+	 *numbers before it aswell as itself. This cuts down on time and
+	 *is extremely efficient as appose to linear search. As numbers
+	 *keep being disregarded you have a smaller amount of numbers to
+	 *work with making your search eisier.
+	 *
+	 *	This strategy would not be possible it hints were not given
+	 *as you wouldnt be able to tell which numbers to disregard. You
+	 *would have to conduct linear search instead of binary search.
+	 *This would be horribly inefficient especially if more numbers
+	 *were added. */
+
+
+
+
+
+
+
